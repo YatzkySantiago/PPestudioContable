f28c5fe4839f06c43254ea56b41a4da25073b373
YatzkySantiago <syatz9674@gmail.com>
2026-06-17 17:12:33 -0300

    adjusted text

--- components/pain-points-section.tsx
+++ components/pain-points-section.tsx
@@ -5,7 +5,7 @@ const painPoints = [
   {
     icon: AlertTriangle,
     title: "",
-    text: "Muchas Pymes ignoran herramientas de financiamiento como las Sociedades de Garantías Recíprocas (SGR), Fondos de Garantías Provinciales y Convenios Pronvinciales de tasas preferenciales.",
+    text: "Muchas Pymes ignoran herramientas de financiamiento como las Sociedades de Garantías Recíprocas (SGR), Fondos de Garantías Provinciales y Convenios Provinciales de tasas preferenciales.",
     image: "/pain1A.webp",
     imageTop: "/pain1B.webp",
     alt: "Equipos de trabajo de Pymes de servicios realizando sus tareas",
